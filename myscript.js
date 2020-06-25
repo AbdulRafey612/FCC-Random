e8030189d1c6d0d8f98f0f0e74a3c4fa82a7ee5b
Abdul Rafey <rafey623@gmail.com>
2020-06-25 11:41:14 +0500

    XML request is working

--- myscript.js
+++ myscript.js
@@ -1,10 +1,27 @@
+let resp;
+let org_resp;
+const random_num = ()=>{
+	let num = Math.floor(Math.random() * 101);
+	return num;
+};	
 function Hello(){
+	console.log("Execution Begun");
 let xhr = new XMLHttpRequest();
-console.log(xhr.readyState);
+xhr.onreadystatechange = ()=>{
+	console.log(xhr.readyState);
+	if(xhr.readyState == 4){
+		resp = xhr.response; 
+		org_resp = JSON.parse(resp);
+		let r_num = random_num();
+		document.getElementById('text').innerHTML = org_resp.quotes[r_num].quote;
+		document.getElementById('author').innerHTML = org_resp.quotes[r_num].author;
+				
+	}
+};
 xhr.open("GET","https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",true);
-console.log(xhr.readyState);
-console.log(document.getElementById("demo"));	
-console.log(document.getElementById("demo"));
+xhr.send();
 }
-setTimeout(Hello,3000);
 
+$('document').ready(Hello());
+
+			
